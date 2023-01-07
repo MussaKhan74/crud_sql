8318825c18a5cb80b06dc96c7a7d4d37955722b3
MussaKhan74 <MarkeLoof@gmail.com>
2023-01-07 23:30:40 +0500

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.json("hello from the backend!");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `price`,`cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully!");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id =?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted successfully!");
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated successfully!");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend!");
+});
